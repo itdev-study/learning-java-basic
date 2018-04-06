899a9451cf09ae39a9c024b2aa4ab0028e7b5cd1
o.levitskiy <o.levitskiy@GR-MS-IT03.ipmatika.local>
2018-04-06 11:29:27 +0300

    FirstAlfa-0.1

---
+++ learning-java-basic/src/learning/java/basic/LearningJavaBasic.java
@@ -0,0 +1,12 @@
+/*
+ * learning.java.basic
+ */
+package learning.java.basic;
+
+public class LearningJavaBasic {
+
+    public static void main(String[] args) {
+        System.out.println("TEST");
+    }
+    
+}
